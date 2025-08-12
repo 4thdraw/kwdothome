1182eb52ea19277956577750a7ac1a73beae4cda
4thdraw <4thdraw2@gmail.com>
2025-08-12 17:29:10 +0900

    공지사항 컨텐츠

--- page/board_list.php
+++ page/board_list.php
@@ -9,16 +9,88 @@ include_once("../config.php"); ?>
 
 <!--게시판 리스트-->		
 
+ <!-- 제품하위카테고리 -->
+ <div class='subMn py-5'>
+			<div class="max-width mx-auto px-3 px-xl-0 d-flex w-100 justify-content-between align-items-center pt-0">
+				<div>
+					<h2 class='page_title fs-48 fw400 backgroundnone'>
+							<span >
+								<?php echo $board_title; ?>
+							</span>
+							
+							<strong class='active-color fw400 text-uppercase'>
+							    <?php echo $board_title_en; ?>
+							</strong>
+					</h2> 
+					<p class='fs-20 gray-dark mt-3'>
+						고객 공간에 최적화된 맞춤 시공으로 완성된<br>
+                        다양한 카페트 사례를 만나보세요.
+					</p>
+				</div>
+					
+				<!--검색-->
+				<div class='kw_gallery_searchdom'>
+					<form name="frmSearch" id="frmSearch" class="d-flex align-items-center gap-3 "  method="GET" onSubmit="return searchForm();">
+							
+							
+
+						
+							<div class='border border-dark-gray  px-3 rounded-3 d-flex align-items-center h43 '>
+								<input type="text" name="sbval" id="sbval" maxlength="50" class='border-0'  
+								placeholder="검색어를 입력해주세요."   />
+								<input type="image" src="../img/2025/ui/search_icon.png"  />
+							</div>
+							
+					</form>
+			  	</div>
+			
+				<!--//검색-->
+			</div>
+	 </div>
 
 
-<div class='max-width d-flex mx-auto  position-relative mainContentwrapper'  >
-	<div class='kw_wrap d-flex flex-column align-items-center max-width mx-auto'>
-		<h2 class='page_title'>
-		<?php echo $board_title; ?>
-		<strong>
-			<?php echo $board_title_en; ?>
-		</strong>
-		</h2> 
+<div class='max-width d-flex mx-auto  position-relative mainContentwrapper mb-5'  >
+	<div class='kw_wrap w-100'>
+	<!-- 가상DB // 테이블이 같은 구조일 것으로 예상하여 하나로 매트상식,공지사항, 견적문의 스킨만 다르게 진행-->
+	<?php include_once($kw_path."/inc/db/inquiries_list.php"); ?>
+
+		<ul class="inquiry-list board-list  fs-16 fw600 ">
+		       <li class='d-flex  justify-content-between active-bg text-white px-3'>
+					<div class="inquiry-header col-2 py-3">
+						<span >NO.</span>
+						<span>분류</span>						
+					</div>
+
+					<div class="inquiry-title flex-grow-1 line-height-1-6 d-flex align-items-center">제목</div>
+
+					<div class="inquiry-meta d-flex col-4 align-items-center">
+						
+						<span>작성자</span>
+						<span>작성일</span>
+						<span>조회수</span>
+						<span>상태</span>
+					</div>
+				</li>
+
+			<?php foreach ($inquiries as $inq): ?>
+				<li class='d-flex justify-content-between px-3'>
+					<div class="inquiry-header  col-2 py-3">
+						<span><?= $inq['no'] ?></span>
+						<span class="status <?= $inq['status'] ?>"><?= $inq['status'] ?></span>
+					</div>
+
+					<div class="inquiry-title inquiry-title flex-grow-1 line-height-1-6 "><?= htmlspecialchars($inq['title']) ?></div>
+
+					<div class="inquiry-meta d-flex col-4">
+						<span><?= $inq['category'] ?></span>
+						<span>작성자: <?= $inq['author'] ?></span>
+						<span>작성일: <?= $inq['date'] ?></span>
+						<span>조회수: <?= $inq['views'] ?></span>
+					</div>
+				</li>
+			<?php endforeach; ?>
+		</ul>
+	  
 	</div>
 	<?php include $kw_path."/inc/quick_sticky.php"; ?>
 </div>
--- page/gallery_list.php
+++ page/gallery_list.php
@@ -471,8 +471,6 @@ $tmp_qstring = $CONF['query_string'];
 $row_list_count = count($row_list);
 ?>
 
-
-<div class=' position-relative content_wrapper mainContentwrapper'  >
 	 <!-- 제품하위카테고리 -->
 	 <div class='subMn border-bottom py-5'>
 			<div class="max-width mx-auto px-3 px-xl-0 d-flex w-100 justify-content-between align-items-center pt-0">
@@ -523,23 +521,14 @@ $row_list_count = count($row_list);
 				<!--//검색-->
 			</div>
 	 </div>
+
+<div class=' position-relative content_wrapper mainContentwrapper'  >
+
 	 <!-- 갤러리 게시판 목록			 -->
 
 	<div>
-
-
-
-	
-	
 	<!--서브컨텐츠-->
-		<div class="contents max-width mx-auto px-3 px-xl-0"  >
-					
-					
-					
-						
-						
-			
-			
+		<div class="contents max-width mx-auto px-3 px-xl-0"  >				
 			<!--게시판-->
 			<?php if($class_name_big!=''){ ?>
 			<div class='board_gallery'>				
@@ -604,7 +593,7 @@ $row_list_count = count($row_list);
 							<path d="M1.11469 7.50024L6.90094 13.2872C7.01094 13.3972 7.06544 13.5292 7.06444 13.6832C7.06344 13.8372 7.00744 13.9695 6.89644 14.08C6.78544 14.1905 6.65319 14.2457 6.49969 14.2457C6.34619 14.2457 6.21394 14.1905 6.10294 14.08L0.372188 8.35299C0.251188 8.23149 0.162687 8.09649 0.106687 7.94799C0.0511875 7.79849 0.0234375 7.64924 0.0234375 7.50024C0.0234375 7.35124 0.0511875 7.20224 0.106687 7.05324C0.162687 6.90424 0.251188 6.76924 0.372188 6.64824L6.10219 0.915239C6.21269 0.804739 6.34569 0.750239 6.50119 0.751739C6.65619 0.753239 6.78919 0.809239 6.90019 0.919739C7.01019 1.03024 7.06519 1.16249 7.06519 1.31649C7.06519 1.47049 7.01019 1.60274 6.90019 1.71324L1.11469 7.50024Z" fill="black"/>
 						</svg>
 					</span>
-					<?php echo $FUNC->getPageIndexH($CONF['request_uri'], $CONF['query_flag'], $cpage, $CONF['normal_page_limit'], $total_article)?>
+					<?php echo $FUNC->getPageIndexH($CONF['request_uri'], $CONF['query_flag'], $cpage, $CONF['normal_page_limit'], $total_article); ?>
 					<span class='next_arrow d-flex flex-column justify-content-center px-2'>
 						<svg width="8" height="15" viewBox="0 0 8 15" fill="none" xmlns="http://www.w3.org/2000/svg">
 							<path d="M6.88531 7.50025L1.09906 1.71325C0.989062 1.60325 0.934562 1.47125 0.935562 1.31725C0.936562 1.16325 0.992562 1.031 1.10356 0.9205C1.21456 0.81 1.34681 0.754751 1.50031 0.754751C1.65381 0.754751 1.78606 0.81 1.89706 0.9205L7.62781 6.6475C7.74881 6.769 7.83731 6.904 7.89331 7.0525C7.94881 7.202 7.97656 7.35125 7.97656 7.50025C7.97656 7.64925 7.94881 7.79825 7.89331 7.94725C7.83731 8.09625 7.74881 8.23125 7.62781 8.35225L1.89781 14.0852C1.78731 14.1957 1.65431 14.2502 1.49881 14.2487C1.34381 14.2472 1.21081 14.1912 1.09981 14.0807C0.989811 13.9702 0.934812 13.838 0.934812 13.684C0.934812 13.53 0.989811 13.3977 1.09981 13.2872L6.88531 7.50025Z" fill="black"/>
@@ -612,10 +601,8 @@ $row_list_count = count($row_list);
 					</span>
 			</div>
         </div>
-	</div>
-	<!--//서브컨텐츠--> 
-
-	</div>
+	
+	
 	<!--//컨텐츠-->
 		<?php $SQL->close(); ?>
 	</div>	
